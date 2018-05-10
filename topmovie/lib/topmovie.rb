2f5c05477cc5819e6f783432d34830ee2766c4d0
Eric Johnson <ericjohnson0216@icloud.com>
2018-05-10 00:04:20 +0000

    Done.

---
+++ topmovie/lib/topmovie.rb
@@ -0,0 +1,13 @@
+
+# require "topmovies/version"
+#
+# module Topmovies
+#   # Your code goes here...
+# end
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
+
+require_relative './topmovies/cli'
+require_relative './topmovies/movies'
+require_relative './topmovies/version'
---
+++ topmovie/lib/topmovie/version.rb
@@ -0,0 +1,3 @@
+module Topmovie
+  VERSION = "0.1.0"
+end
