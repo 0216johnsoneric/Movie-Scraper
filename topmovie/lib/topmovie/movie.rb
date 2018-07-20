2939cc2417acf1491671fb0307ce37f14b2791d2
Eric Johnson <ericjohnson0216@icloud.com>
2018-07-20 00:46:41 +0000

    Done.

--- topmovie/lib/topmovie/movie.rb
+++ topmovie/lib/topmovie/movie.rb
@@ -1,17 +1,16 @@
 class Movie
   @@all = []
   attr_accessor :name, :gross, :weeks
-  # Movie class initialize method:
 
-  def initialize (name, gross, weeks)
+  def initialize (name=nil, gross=nil, weeks=nil)
     @name = name
     @gross = gross
     @weeks = weeks
     @@all << self
   end
 
+
   def self.movies
-    # @@all << Scraper.scrape_imdb
     @@all
   end
 
@@ -20,14 +19,10 @@ class Movie
     puts ""
     puts "Type the 'Number' of the movie you would like information on or Type 'Exit' to exit the application."
     puts ""
-    movies = Scraper.today
+    movies = Movie.movies
     movies.each.with_index(1) do |movie, i|
-    puts "#{i}. Name- #{movie.name} Gross Revenue- #{movie.gross} Weeks in Boxoffice- #{movie.weeks}"
+    puts "#{i}. Name - #{movie.name}  |||  Gross Revenue - #{movie.gross}  |||  Weeks in Boxoffice - #{movie.weeks}"
 
     end
   end
-
-  # def self.all
-  #   @@all
-  # end
 end
--- topmovie/lib/topmovie/scraper.rb
+++ topmovie/lib/topmovie/scraper.rb
@@ -1,51 +1,15 @@
 class Scraper
-  attr_accessor :name, :gross, :weeks
   @@doc = Nokogiri::HTML(open("https://www.imdb.com/chart/boxoffice/"))
 
-  def self.today
-    objects = self.scrape_imdb
-    array = objects.first(5)
-    array
-  end
-
-
-
   def self.scrape_imdb
-    # here is what I'm looking for
-    # @@doc.each
-    #   name = ...
-    #   gross = ...
-    #   weeks = ...
-    #   movie = Movie.new(name, gross, weeks)
+    movies = @@doc.css('table[data-caller-name="chart-boxoffice"] tr')
+    movies.shift
+    movies.each do|movie|
 
-    movie_1= self.new
-    movie_1.name = @@doc.css('td.titleColumn')[0].text.strip
-    movie_1.gross = @@doc.css('span.secondaryInfo')[0].text.strip
-    movie_1.weeks = @@doc.css('td.weeksColumn')[0].text.strip
-
-
-    movie_2= self.new
-    movie_2.name = @@doc.css('td.titleColumn')[1].text.strip
-    movie_2.gross = @@doc.css('span.secondaryInfo')[1].text.strip
-    movie_2.weeks = @@doc.css('td.weeksColumn')[1].text.strip
-
-
-    movie_3= self.new
-    movie_3.name = @@doc.css('td.titleColumn')[2].text.strip
-    movie_3.gross = @@doc.css('span.secondaryInfo')[2].text.strip
-    movie_3.weeks = @@doc.css('td.weeksColumn')[2].text.strip
-
-    movie_4= self.new
-    movie_4.name = @@doc.css('td.titleColumn')[3].text.strip
-    movie_4.gross = @@doc.css('span.secondaryInfo')[3].text.strip
-    movie_4.weeks = @@doc.css('td.weeksColumn')[3].text.strip
-
-    movie_5= self.new
-    movie_5.name = @@doc.css('td.titleColumn')[4].text.strip
-    movie_5.gross = @@doc.css('span.secondaryInfo')[4].text.strip
-    movie_5.weeks = @@doc.css('td.weeksColumn')[4].text.strip
-
-    [movie_1, movie_2, movie_3, movie_4, movie_5]
-
+      new_movie = Movie.new
+      new_movie.name = movie.css('td.titleColumn').text.strip
+      new_movie.gross = movie.css('span.secondaryInfo').text.strip
+      new_movie.weeks = movie.css('td.weeksColumn').text.strip
+    end
   end
 end
