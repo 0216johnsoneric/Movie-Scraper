6cc57241503213e6f5b53d564abfce9a54e2a960
Eric Johnson <ericjohnson0216@icloud.com>
2018-07-30 22:40:07 +0000

    Done.

--- topmovie/lib/topmovie/cli.rb
+++ topmovie/lib/topmovie/cli.rb
@@ -14,13 +14,12 @@ class Topmovie::Cli
     list_movies
     input = nil
     while input != "exit"
-      # list_movies
       prompt
       input = gets.strip.downcase
 
 
 
-      if input.to_i > 0 && input.to_i <= Topmovie::Movie.all.count 
+      if input.to_i > 0 && input.to_i <= Topmovie::Movie.all.count
         the_movie_listing = Topmovie::Movie.all[input.to_i-1]
         puts "Movie Name: #{the_movie_listing.name}"
         puts "Gross Revenue: #{the_movie_listing.gross}"
@@ -33,7 +32,7 @@ class Topmovie::Cli
       elsif input.downcase == 'list'
         list_movies
       else
-        
+
         puts " I don't understand your input."
       end
     end
@@ -42,17 +41,15 @@ class Topmovie::Cli
   def list_movies
     puts "This weeks Top Box Office Movies:"
     puts ""
-    # puts "Type the 'Number' of the movie you would like information on or Type 'Exit' to exit the application."
-    # puts ""
     movies = Topmovie::Movie.all
     movies.each.with_index(1) do |movie, i|
       puts "#{i}. Name - #{movie.name}"
-    
+
     end
-    
+
   end
 
-  def prompt 
+  def prompt
     puts ""
     puts "Type the 'Number' of the movie you would like information on or Type 'List' to see the list of movies again, or Type 'Exit' to exit the application."
     puts ""
